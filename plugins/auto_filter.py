7d7d43acff27b57060a39113a12c8c30209ecb3d
Christopher Martin <tmajibon@gmail.com>
2015-11-14 15:15:15 -0600

    Added support for regular expressions

--- plugins/auto_filter.py
+++ plugins/auto_filter.py
@@ -7,6 +7,11 @@ Emails are tested first for full match to username@domain before testing only th
 
 Domains are tested in parts, with 1.example.com matching first 1.example.com and then example.com.
 
+In each, rules containing subject matches are checked before rules without subject matches.
+
+(NOTE: Subject field supports regular expressions via the python re package, see
+https://docs.python.org/2/library/re.html for information on how to construct for this format.)
+
 First match found is the first applied.
 
 options:
@@ -23,13 +28,15 @@ options:
     | domain   | varchar(255)     | YES  | MUL | NULL    |                |
     | seen     | tinyint(1)       | YES  |     | 0       |                |
     | folder   | varchar(255)     | YES  | MUL | NULL    |                |
+    | subject  | varchar(255)     | YES  |     | NULL    |                |
     +----------+------------------+------+-----+---------+----------------+
     5 rows in set (0.26 sec)
 
 """
 
-from sqlalchemy import Table, Column, Integer, String, Boolean, Index
+from sqlalchemy import Table, Column, Integer, String, Boolean, Index, or_
 import __filter
+import re
 
 
 class mailfilter(__filter.mailfilter):
@@ -45,6 +52,7 @@ class mailfilter(__filter.mailfilter):
                                  Column('domain', String(255), index=True),
                                  Column('seen', Boolean, default=False),
                                  Column('folder', String(255), index=True),
+                                 Column('subject', String(255)),
                                  __table_args__=(
                                      Index('filter_address', 'username', 'domain'),
                                  )
@@ -57,22 +65,40 @@ class mailfilter(__filter.mailfilter):
             if not handle.folder_exists(result.folder):
                 handle.create_folder(result.folder)
 
+    def __check_rules(self,header,rules):
+        for rule in rules:
+            if rule.subject:
+                if re.search(rule.subject, header["Subject"]):
+                    return rule
+            else:
+                return rule
+
+        return None
+
+
     def filter(self, handler, msgid, header):
+        basequery = self.dbsession.query(self.auto_filter).\
+            order_by(
+                self.auto_filter.c.username.desc(),
+                self.auto_filter.c.subject.desc()
+            )
+
         address = header['From'].split('<')[-1].split('>')[0].strip().lower()
         username, domain = address.split('@')
 
-        result = self.dbsession.query(self.auto_filter).filter_by(username=username, domain=domain).first()
+        filter_match = False
 
-        if not result:
-            domainparts = domain.split('.')
-            for i in range(-(len(domainparts)), 0):
-                testdomain = '.'.join(domainparts[i:len(domainparts)])
-                self.loghandler.output("Testing %s" % testdomain, 10)
+        results = basequery.filter_by(username=username, domain=domain).all()
 
-                result = self.dbsession.query(self.auto_filter).filter_by(username=None, domain=domain).first()
+        domainparts = domain.split('.')
+        for i in range(-(len(domainparts)), 0):
+            testdomain = '.'.join(domainparts[i:len(domainparts)])
+            self.loghandler.output("Testing %s" % testdomain, 10)
 
-                if result:
-                    break
+            results.extend(basequery.filter_by(domain=testdomain).all())
+
+        result = self.__check_rules(header, results)
+
 
         if result:
             if result.seen:
